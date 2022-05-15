456ecc9dca8c820deca365f0c53b7f6dfd38be27
Paweł Cetnarowski <studentpawel2000@gmail.com>
2022-05-15 15:40:08 +0200

    model klasy + kontroler, rozpoczęcie implementacji planu zajęć oraz uprawnień

---
+++ database/seeders/SchoolClassesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\SchoolClass;
+use Illuminate\Database\Seeder;
+
+class SchoolClassesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $classes = [
+            [
+                'id' => 1,
+                'name' => '1 klasa'
+            ],
+            [
+                'id' => 2,
+                'name' => '2 klasa'
+            ],
+            [
+                'id' => 3,
+                'name' => '3 klasa'
+            ],
+            [
+                'id' => 4,
+                'name' => '4 klasa'
+            ],
+            
+
+        ];
+
+        SchoolClass::insert($classes);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,32 @@ Route::get('/home', function () {
 });
 
 
-Auth::routes();
+Auth::routes(['register' => false]);
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    // Permissions
+    //Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+   // Route::resource('permissions', 'PermissionsController');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+    // Lessons
+    Route::delete('lessons/destroy', 'LessonsController@massDestroy')->name('lessons.massDestroy');
+    Route::resource('lessons', 'LessonsController');
+
+    // School Classes
+    Route::delete('school-classes/destroy', 'SchoolClassesController@massDestroy')->name('school-classes.massDestroy');
+    Route::resource('school-classes', 'SchoolClassesController');
+
+    Route::get('calendar', 'CalendarController@index')->name('calendar.index');
+});
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
