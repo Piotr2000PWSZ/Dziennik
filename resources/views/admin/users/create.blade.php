ab1368084ce3aac417ca4c663c44e4a25c722cc5
Piotr Bożek <piotrek267@onet.pl>
2022-05-24 13:24:46 +0200

    Dodawanie i usuwanie uzytkowników

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        Utwórz użytkownika
+    </div>
+
+    <div class="card-body">
+        <form method="POST" action="{{ route("admin.users.store") }}" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label class="required" for="name">Nazwa</label>
+                <input class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" type="text" name="name" id="name" value="{{ old('name', '') }}" required>
+                @if($errors->has('name'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('name') }}
+                    </div>
+                @endif
+                
+            </div>
+            <div class="form-group">
+                <label class="required" for="email">E-mail</label>
+                <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="text" name="email" id="email" value="{{ old('email') }}" required>
+                @if($errors->has('email'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('email') }}
+                    </div>
+                @endif
+                
+            </div>
+            <div class="form-group">
+                <label class="required" for="password">Hasło</label>
+                <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password" id="password" required>
+                @if($errors->has('password'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('password') }}
+                    </div>
+                @endif
+            </div>
+            @if(!request()->has('student'))
+                <div class="form-group">
+                    <label class="required" for="roles">Rola</label>
+                    
+                    <select class="form-control select2 {{ $errors->has('roles') ? 'is-invalid' : '' }}" name="roles[]" id="roles" multiple required>
+                        @foreach($roles as $id => $roles)
+                            <option value="{{ $id }}" {{ in_array($id, old('roles', [])) ? 'selected' : '' }}>{{ $roles }}</option>
+                        @endforeach
+                    </select>
+                    @if($errors->has('roles'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('roles') }}
+                        </div>
+                    @endif
+                    
+                </div>
+            @else
+                <input type="hidden" name="roles[]" value="4">
+            @endif
+            <div class="form-group">
+                <label for="class_id">Wybierz klasę</label>
+                <select class="form-control select2 {{ $errors->has('class') ? 'is-invalid' : '' }}" name="class_id" id="class_id">
+                    @foreach($classes as $id => $class)
+                        <option value="{{ $id }}" {{ old('class_id') == $id ? 'selected' : '' }}>{{ $class }}</option>
+                    @endforeach
+                </select>
+                @if($errors->has('class'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('class') }}
+                    </div>
+                @endif
+                
+            </div>
+            <div class="form-group">
+                <button class="btn btn-danger" type="submit">
+                    Utwórz
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
+
+@endsection
